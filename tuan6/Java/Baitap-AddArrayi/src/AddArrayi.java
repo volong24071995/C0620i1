be6e819a588b826442005cc2069f56f65f310399
unknown <volong24071995@gmail.com>
2020-08-10 08:06:28 +0700

    update Java

--- tuan6/Java/Baitap-AddArrayi/src/AddArrayi.java
+++ tuan6/Java/Baitap-AddArrayi/src/AddArrayi.java
@@ -2,44 +2,45 @@ import java.util.Scanner;
 
 public class AddArrayi {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter size: ");
-        int size=check();
-        while (size>20||size<0){
+        int size = check();
+        while (size > 20 || size < 0) {
             System.out.println("Enter 0<size<=20: ");
-            size=check();
+            size = check();
         }
-        int[] arr=new int[size];
-        for (int i=0;i<size;i++){
-            arr[i]=(int)(Math.random()*100-50);
-            System.out.printf(arr[i]+", ");
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = (int) (Math.random() * 100 - 50);
+            System.out.printf(arr[i] + ", ");
         }
         System.out.println();
 //        NHAP INDEX
         System.out.println("Enter index: ");
-        int index=check();
-        while(index>size||index<0){
-            System.out.println("Enter 0<=index<"+size+": ");
-            index=check();
+        int index = check();
+        while (index > size || index < 0) {
+            System.out.println("Enter 0<=index<=" + size + ": ");
+            index = check();
         }
 //        NHAP VALUE
         System.out.println("Enter value ");
-        int value=check();
+        int value = check();
 
 //        TINH
-        int[] newarr=new int[size+1];
-        for (int i=size;i>=0;i--){
-            if (i>index){
-                newarr[i]=arr[i-1];
-            }else if(i==index){
-                newarr[index]=value;
-            }else  newarr[i]=arr[i];
+        int[] newarr = new int[size + 1];
+        for (int i = size; i >= 0; i--) {
+            if (i > index) {
+                newarr[i] = arr[i - 1];
+            } else if (i == index) {
+                newarr[index] = value;
+            } else newarr[i] = arr[i];
         }
-        for (int i=0;i< newarr.length;i++){
-            System.out.printf(newarr[i]+", ");
+        for (int i = 0; i < newarr.length; i++) {
+            System.out.printf(newarr[i] + ", ");
         }
     }
-//      ham kiem tra so nguyen
+
+    //      ham kiem tra so nguyen
     public static int check() {
         Scanner sc = new Scanner(System.in);
         int n = 0;
@@ -54,5 +55,6 @@ public class AddArrayi {
             }
         }
         return n;
+
     }
 }
--- tuan6/Java/Baitaplamthem-OOP/src/Persson.java
+++ tuan6/Java/Baitaplamthem-OOP/src/Persson.java
@@ -3,30 +3,43 @@ import java.util.Scanner;
 public class Persson {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter table: ");
-        int numtable=scanner.nextInt();
+        int[] table=new int[10];
         System.out.println("Menu");
         System.out.println("1. Nuoc cam");
         System.out.println("2. Nuoc loc");
         System.out.println("3. Nuoc Oi");
         System.out.println("0.Chua muon uong");
+
+        System.out.println("Enter table: ");
+        int number=scanner.nextInt();
+
         int count=0;
+        System.out.println("Enter mon");
         while (true){
-            System.out.println("Enter mon");
+
             int sttmon=scanner.nextInt();
+            int sl = 0;
             switch (sttmon){
                 case 1:
-                    count++;
-                    System.out.println("Nuoc cam");
+                    int pire1=2000;
+                    System.out.println("Nuoc cam "+pire1+"/ly");
+                    System.out.printf("So luong:");
+                    sl = scanner.nextInt();
+                    count+=sl;
                     break;
                 case 2:
-                    count++;
-                    System.out.println("Nuoc loc");
+                    int pire2=3000;
+                    System.out.println("Nuoc loc "+pire2+"/chai");
+                    System.out.printf("So luong:");
+                    sl = scanner.nextInt();
+                    count+=sl;
                     break;
                 case 3:
-                    count++;
-                    System.out.println("Nuoc Oi");
-                    System.out.println(count);
+                    int pire3=4000;
+                    System.out.println("Nuoc Oi "+pire3+"/ly");
+                    System.out.printf("So luong:");
+                    sl = scanner.nextInt();
+                    count+=sl;
                     break;
                 case 0:
                     System.out.println("Chua muon uong");
