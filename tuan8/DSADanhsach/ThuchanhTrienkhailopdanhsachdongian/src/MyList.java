9adc921cc29f40e4b2a00b08cc496a900c69501d
unknown <volong24071995@gmail.com>
2020-08-18 21:25:06 +0700

    up

--- tuan8/DSADanhsach/ThuchanhTrienkhailopdanhsachdongian/src/MyList.java
+++ tuan8/DSADanhsach/ThuchanhTrienkhailopdanhsachdongian/src/MyList.java
@@ -21,11 +21,76 @@ public class MyList<E> {
         }
         elements[size++] = e;
     }
+
+    public void add(E e,int index) {
+        if (index>elements.length) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index );
+        }else if(index==elements.length){
+            ensureCapa();
+        }
+        if (elements[index]==null){
+            elements[index]=e;
+            size++;
+        }else {
+            for (int i=size+1;i>=index;i--){
+                elements[i]=elements[i-1];
+            }
+            elements[index]=e;
+            size++;
+        }
+        elements[size++] = e;
+    }
+
     public E get(int i) {
         if (i>= size || i <0) {
             throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i );
         }
         return (E) elements[i];
+    }
+
+    private int size(){
+        return this.size;
+    }
+
+    public void clear(){
+        this.size=0;
+        for (int i=0;i< elements.length;i++){
+            elements[i]=null;
+        }
+    }
+
+    public int indexOf(E e){
+        int index=-1;
+        for (int i=0;i<size;i++){
+            if (elements[i].equals(e)){
+               return i;
+            }
+        }
+        return index;
+    }
+
+    public boolean contains(E e){
+            return indexOf(e)>=0;
+    }
+
+    public MyList<E> clone(){
+        MyList<E> v=new MyList<>();
+        v.elements=Arrays.copyOf(elements, size);
+        v.size=this.size;
+        return v;
+    }
+
+    public E remove(int index){
+        if (index>= size || index <0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index );
+        }
+        E element=(E) elements[index];
+        for (int i=0;i<size-1;i++){
+            elements[i]=elements[i+1];
+        }
+        elements[size-1]=null;
+        size--;
+        return element;
     }
 }
 
--- tuan8/DSADanhsach/ThuchanhTrienkhailopdanhsachdongian/src/MyListTest.java
+++ tuan8/DSADanhsach/ThuchanhTrienkhailopdanhsachdongian/src/MyListTest.java
@@ -13,12 +13,10 @@ public class MyListTest {
         listInteger.add(4);
         listInteger.add(1);
         listInteger.add(2);
-        
-        listInteger.add(3);
-        listInteger.add(3);
-        listInteger.add(4);
+        listInteger.add(20,4);
+        listInteger.remove(2);
 
-
+        System.out.println(listInteger.contains(20));
         System.out.println("element 4: " + listInteger.get(4));
         System.out.println("element 1: " + listInteger.get(1));
         System.out.println("element 2: " + listInteger.get(2));
