c0ac00a6082b76d270915d4ba58ee76828325dc3
unknown <volong24071995>
2020-10-27 08:59:54 +0700

    updateORM

---
+++ Modul4/ORM/QLDSKH/cms/src/main/java/cg/wbd/grandemonstration/service/impl/HibernateCustomerServiceImpl.java
@@ -0,0 +1,160 @@
+package cg.wbd.grandemonstration.service.impl;
+
+import cg.wbd.grandemonstration.model.Customer;
+import cg.wbd.grandemonstration.service.CustomerService;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class HibernateCustomerServiceImpl implements CustomerService {
+    private static SessionFactory sessionFactory;
+    private static EntityManager entityManager;
+    public static long id=0;
+    static {
+        try {
+             sessionFactory = new Configuration()
+                    .configure("hibernate.conf.xml")
+                    .buildSessionFactory();
+            sessionFactory.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static {
+        try {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.conf.xml")
+                    .buildSessionFactory();
+            entityManager = sessionFactory.createEntityManager();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        String queryStr = "SELECT c FROM Customer AS c";
+        TypedQuery<Customer> query = entityManager.createQuery(queryStr, Customer.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public Customer findOne(Long id) {
+         String queryStr = "SELECT c FROM Customer AS c WHERE c.id = :id";
+        TypedQuery<Customer> query = entityManager.createQuery(queryStr, Customer.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            Customer origin = findOne(customer.getId());
+            origin.setName(customer.getName());
+            origin.setEmail(customer.getEmail());
+            origin.setAddress(customer.getAddress());
+            session.saveOrUpdate(origin);
+            transaction.commit();
+            return origin;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return null;
+    }
+    @Override
+    public Customer add(Customer customer) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.saveOrUpdate(customer);
+            transaction.commit();
+            return customer;
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Customer> save(List<Customer> customers) {
+        return null;
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> findAll(List<Long> ids) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(Long id) {
+    }
+
+    @Override
+    public void delete(Customer customer) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.delete(customer);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public void delete(List<Customer> customers) {
+    }
+
+    @Override
+    public void deleteAll() {
+    }
+}
